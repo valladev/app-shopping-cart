7bd3ba841e058a82878dd4d30aa2d4cf62c5d81e
João L. Barboza <105928845+valladev@users.noreply.github.com>
2023-07-28 13:29:07 -0300

    add Tailwind in the project

---
+++ components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <>
+      <header>
+        <div className=''>
+
+        </div>
+      </header>
+    </>
+  );
+
+}
